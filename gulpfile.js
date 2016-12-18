a781e04b52c9d6eb6b678b7043b14a27ad055b42
KHANH QUANG <KHANH QUANG>
2016-12-19 02:08:45 +0700

    ignore node_modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
 var gulp = require('gulp');
+
 var sass = require('gulp-sass');
+
 var browserSync = require('browser-sync').create();
+
 var livereload = require('gulp-livereload');
+
 var notify = require('gulp-notify');
+
+var autoprefix = require('autoprefixer');
+
+var postcss = require('gulp-postcss');
+
+var lost = require('lost');
+
+var sourcemaps = require('gulp-sourcemaps');
+
+
+
+
 //sass
 
 function swallowError (error) {
@@ -24,8 +40,12 @@ gulp.task('sass', function() {
 					message: 'Error'
 				})
 			}
-		}))
+		}))		
 		.on('error', swallowError)
+		.pipe(postcss([
+				lost(),
+				autoprefix()
+			]))
 		.pipe(gulp.dest('./css'))
 		.pipe(livereload());
 });
@@ -36,11 +56,24 @@ gulp.task('html', function() {
 		.pipe(livereload());
 });
 
+//javascript
+gulp.task('js', function() {
+	gulp.src('./js/**/*.js')
+		.pipe(livereload());
+});
+
+// reload sass file
+gulp.task('sass-reload', function() {
+	gulp.src('./src/css/**/*.sass')
+		.pipe(livereload());
+})
+
 
 // watch change
-gulp.task('watch',['sass'], function() {
+gulp.task('watch',['sass', 'js', 'html'], function() {
 	gulp.watch('./src/css/**/*.sass', ['sass']);
 	gulp.watch('./**/*.html', ['html']);
+	gulp.watch('./js/**/*.js', ['js']);
 
 	livereload.listen({
 		host: "localhost",
