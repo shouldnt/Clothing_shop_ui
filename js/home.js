b67abfea98e4eb4b2e53bdc0c3deae52efc304e0
KHANH QUANG <KHANH QUANG>
2016-12-19 16:07:05 +0700

    Home page 85% complete - header - nav - footer -  products card 95%

--- js/home.js
+++ js/home.js
@@ -1,4 +1,30 @@
-new Vue({
-	el: '.products',
-	data: {}
+Vue.component('img-slider', {
+  template: '#img-slider-template',
+  replace: true
+})
+
+var slider = new Vue({
+	el: '.slide-container'
+})
+
+var product = new Vue({
+	el: '#content__right',
+	data: {
+		show: false,
+		show_more: {
+			height: '350px',
+			overflow: 'hidden'
+		},
+		show_less: {
+			height: 'auto',
+			overflow: 'visible'
+		}
+	},
+
+	methods: {
+		toggle: function() {
+			this.show = !this.show;
+		}
+	}
+
 })
