716dbe1c70e518fa87d24476d373f588b413cf0e
KHANH QUANG <KHANH QUANG>
2017-01-12 14:07:42 +0700

    vest nu data

---
+++ js/data/vest_nu.js
@@ -0,0 +1,183 @@
+var vest_nu = {
+	cloths: [
+		{
+			id: 1,
+			img: "/img/1.jpg",
+			name: "bộ vest nữ công sở chuyên nghiệp",
+			price: 1030000,
+			sale: 20,
+			status: true
+		},
+		{
+			id: 2,
+			img: "/img/2.jpg",
+			name: "áo vest cổ ve bẻ thu đông",
+			price: 695000,
+			sale: 20,
+			status: true
+		},
+
+		{
+			id: 3,
+			img: "/img/3.jpg",
+			name: "vest nữ thời trang thu đông kiểu không cổ",
+			price: 695000,
+			sale: 20,
+			status: true
+		},
+
+		{
+			id: 4,
+			img: "/img/4.jpg",
+			name: "Áo khoác vest thu đông (be/đen/trắng)",
+			price: 695000,
+			sale: 20,
+			status: true
+		},
+
+		{
+			id: 5,
+			img: "/img/5.jpg",
+			name: "áo khoác vest thời trang thu đông",
+			price: 695000,
+			sale: 20,
+			status: true
+		},
+
+		{
+			id: 6,
+			img: "/img/6.jpg",
+			name: "áo khoác vest",
+			price: 695000,
+			sale: 20,
+			status: true
+		},
+
+		{
+			id: 7,
+			img: "/img/7.jpg",
+			name: "vest thời trang thu đông",
+			price: 695000,
+			sale: 20,
+			status: true
+		},
+
+		{
+			id: 8,
+			img: "/img/8.jpg",
+			name: "vest bộ màu đen và tím thang",
+			price: 1030000,
+			sale: 20,
+			status: true
+		},
+
+		{
+			id: 9,
+			img: "/img/9.jpg",
+			name: "bộ vest công sở nữ phong cách hàn quốc",
+			price: 1030000,
+			sale: 20,
+			status: true
+		},
+
+		{
+			id: 10,
+			img: "/img/10.jpg",
+			name: "Vest trắng hàn quốc thu đông 2017",
+			price: 695000,
+			sale: 20,
+			status: true
+		},
+
+		{
+			id: 11,
+			img: "/img/11.jpg",
+			name: "blazer nữ phong cách hàn quốc thu đông",
+			price: 695000,
+			sale: 20,
+			status: true
+		},
+
+		{
+			id: 12,
+			img: "/img/12.jpg",
+			name: "vest công sở nữ thu đông 2017",
+			price: 695000,
+			sale: 20,
+			status: true
+		},
+
+		{
+			id: 13,
+			img: "/img/13.jpg",
+			name: "vest nữ cổ tròn thu đông 2017",
+			price: 695000,
+			sale: 20,
+			status: true
+		},
+
+		{
+			id: 14,
+			img: "/img/14.jpg",
+			name: "vest trắng nhẹ nhàng mùa thu",
+			price: 695000,
+			sale: 20,
+			status: true
+		},
+
+		{
+			id: 15,
+			img: "/img/15.jpg",
+			name: "bộ vest công sở nữ thu đông 2017",
+			price: 1060000,
+			sale: 20,
+			status: true
+		},
+
+		{
+			id: 16,
+			img: "/img/16.jpg",
+			name: "áo vest lửng thu đông 2017",
+			price: 665000,
+			sale: 20,
+			status: true
+		},
+
+		{
+			id: 17,
+			img: "/img/17.jpg",
+			name: "áo khoác vest dáng dài thu đông 2017",
+			price: 865000,
+			sale: 20,
+			status: true
+		},
+
+		{
+			id: 18,
+			img: "/img/18.jpg",
+			name: "bộ vest nữ công sở phong cách hàn quốc",
+			price: 1060000,
+			sale: 20,
+			status: true
+		},
+
+		{
+			id: 19,
+			img: "/img/19.jpg",
+			name: "blazer công sở nữ thu đông",
+			price: 665000,
+			sale: 20,
+			status: true
+		},
+
+		{
+			id: 20,
+			img: "/img/20.jpg",
+			name: "áo jacket nữ hàn quốc",
+			price: 695000,
+			sale: 20,
+			status: true
+		}
+
+	]
+}
